937b626dd96460dd03ad062280b2169e7c720812
Resek <matija.resek@gmail.com>
2017-06-23 10:53:48 +0200

    first version of app commited

---
+++ quoteMachine.js
@@ -0,0 +1,26 @@
+var newQuoteButton = document.getElementById("newQuoteButton");
+
+newQuoteRequest();
+newQuoteButton.addEventListener("click", newQuoteRequest, false);
+
+function newQuoteRequest() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        var newQuote;
+        var newAuthor;
+        if(xhr.status === 200) {
+            responseObject = JSON.parse(xhr.responseText);        
+            newQuote = responseObject.quote.body;
+            newAuthor = responseObject.quote.author;                              
+        }
+        document.getElementById("quote").innerHTML = newQuote;
+        document.getElementById("author").innerHTML = newAuthor;
+    }
+    xhr.open ("GET", "https://favqs.com/api/qotd", true);
+    xhr.send(null);
+}
+
+
+
+
+
